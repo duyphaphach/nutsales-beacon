568b3885b8c59caf6fe3ce830587b4ec2ead19ef
duyphaphach <duy.pham@genefriendway.com>
2025-01-21 18:48:30 +0700

    Remove secret

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const path = require('path');
 const jwt = require('jsonwebtoken');
 
 const beaconPath = path.join(__dirname, 'assets/icons/nut-beacon.gif');
-const secret = "TdCFWZ7AmhjvaFyTNiQR/+6rC3gYqVv3nsEZ8CPUTgk="
 
 app.get('/api/notifications/emails/beacon/:trackingJwt.gif',
   async function (req, res, next) {
@@ -13,7 +12,7 @@ app.get('/api/notifications/emails/beacon/:trackingJwt.gif',
     try {
       console.log("trackingJwt", trackingJwt);
       // Verify and decode the JWT (trackingJwt)
-      const trackingData = jwt.verify(trackingJwt, secret);
+      const trackingData = jwt.verify(trackingJwt, process.env.AUTH_SECRET);
 
       // Extract required fields from the JWT payload (e.g., userId, emailId, etc.)
       const { userId, emailId } = trackingData;
