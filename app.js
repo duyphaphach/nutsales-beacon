7374c432f3f069030e15816fd63bfb3ea27127b9
duyphaphach <duy.pham@genefriendway.com>
2025-01-22 00:19:54 +0700

    Add Tracking data

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
-const port = 443
+const port = process.env.NODE_ENV === 'development' ? 4003 : 443
 const path = require('path');
 const jwt = require('jsonwebtoken');
+const requestIp = require('request-ip');
+const geoip = require('geoip-lite');
+const accepts = require('accepts');
+const uap = require('ua-parser-js');
 
 const beaconPath = path.join(__dirname, 'assets/icons/nut-beacon.gif');
 
 app.get('/api/notifications/emails/beacon/:trackingJwt.gif',
   async function (req, res, next) {
     const { trackingJwt } = req.params;
+
     try {
       console.log("trackingJwt", trackingJwt);
-      // Verify and decode the JWT (trackingJwt)
-      const trackingData = jwt.verify(trackingJwt, process.env.AUTH_SECRET);
+      const ip = requestIp.getClientIp(req);
+      const geo = geoip.lookup(ip || "");
+      let ua = uap(req.headers['user-agent']);
+      const accept = accepts(req);
 
-      // Extract required fields from the JWT payload (e.g., userId, emailId, etc.)
+      const trackingData = jwt.verify(trackingJwt, process.env.AUTH_SECRET);
       const { userId, emailId } = trackingData;
 
-      console.log('data', { userId, emailId })
-
-      const logEntry = {
-        userId,
-        emailId,
-        timestamp: new Date(),
+      const data = {
+        ip,
+        geo,
+        ua,
+        langs: accept.languages(),
+        charsets: accept.charsets(),
       };
+
+      console.log('trackingData:', JSON.stringify(data, null, 2));
 
       res.sendFile(beaconPath);
     } catch (error) {
